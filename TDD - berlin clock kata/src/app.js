db1da6b46e1e4ee3f4d6b262acfe96eee5a85974
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2023-10-12 17:41:58 +0200

    add:manage case 1

--- TDD - berlin clock kata/src/app.js
+++ TDD - berlin clock kata/src/app.js
@@ -1,21 +1,18 @@
 export class Main {
-    fizzByzz(number) {
+
+    berlinClock(seconde) {
+        if(seconde === 0) return 1;
+    }
+
+
+    /**fizzByzz(number) {
         if(this.isMultipleOfThreeAndFive(number)) return "fizzbuzz";
         if(this.isMultipleOfThree(number)) return "fizz";
         if(this.isMultipleOfFive(number)) return "buzz";
 
         return number;
-    }
+    }**/
 
-    isMultipleOfThreeAndFive(number) {
-        return this.isMultipleOfFive(number) && this.isMultipleOfThree(number);
-    }
 
-    isMultipleOfFive(number) {
-        return number % 5 === 0;
-    }
 
-    isMultipleOfThree(number) {
-        return number % 3 === 0;
-    }
 }
