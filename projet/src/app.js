90571aa6a7e6f73b284d1488cfa5a64346d36659
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2023-11-12 13:07:24 +0100

    creation of the project

---
+++ projet/src/app.js
@@ -0,0 +1,18 @@
+export class Main {
+
+    berlinClock(minute) {
+        if(minute === 0) return "0000" ;
+    }
+
+
+    /**fizzByzz(number) {
+        if(this.isMultipleOfThreeAndFive(number)) return "fizzbuzz";
+        if(this.isMultipleOfThree(number)) return "fizz";
+        if(this.isMultipleOfFive(number)) return "buzz";
+
+        return number;
+    }**/
+
+
+
+}
